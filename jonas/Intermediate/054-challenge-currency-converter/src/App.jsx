fa9b7a81e011a672361b98b1101ab9603c01f144
Manuel Beltran <manuel.beltranc@hotmail.com>
2025-06-27 15:55:56 -0700

    challenge finished

--- jonas/Intermediate/054-challenge-currency-converter/src/App.jsx
+++ jonas/Intermediate/054-challenge-currency-converter/src/App.jsx
@@ -1,55 +1,65 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 export default function App() {
-  const [amount, setAmount] = useState('USD')
-  const [from, setFrom] = useState('EUR')
-  const [to, setTo] = useState('')
-  const [converted, setConverted] = useState('')
+  const [amount, setAmount] = useState(null);
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("EUR");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(
     function () {
       const convert = async () => {
+        setIsLoading(true);
         const res = await fetch(
           `https://api.frankfurter.dev/v1/latest?base=${from}&symbols=${to}`
-        )
-        const data = await res.json()
-        const convertedAmount = (Number(amount) * data.rates[to]).toFixed(2)
-        setConverted(convertedAmount)
+        );
+        const data = await res.json();
+        const convertedAmount = (amount * data.rates[to]).toFixed(2);
+        setConverted(convertedAmount);
+        setIsLoading(false);
+      };
+      if (amount < 0) return;
+      if (from === to) {
+        setConverted(amount);
+        return;
       }
-      convert()
+      convert();
     },
     [amount, from, to]
-  )
+  );
 
   function handleFromSelect(e) {
-    setFrom(e.target.value)
+    setFrom(e.target.value);
   }
 
   function handleToSelect(e) {
-    setTo(e.target.value)
+    setTo(e.target.value);
   }
 
   return (
     <div>
       <input
         type="text"
-        onChange={(e) => setAmount(e.target.value)}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        value={amount}
+        disabled={isLoading}
       />
-      <select onChange={(e) => handleFromSelect(e)}>
+      <select onChange={(e) => handleFromSelect(e)} value={from}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select onChange={(e) => handleToSelect(e)}>
+      <select onChange={(e) => handleToSelect(e)} value={to}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>{converted}</p>
+      <p>{isLoading ? "Calculating..." : converted}</p>
     </div>
-  )
+  );
 }
