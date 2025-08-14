0868ca6a6b600a4d7684e5fa2e7012df143f81b9
Manuel Beltran <manuel.beltranc@hotmail.com>
2025-08-14 00:21:47 -0700

    show ingredients in orders

--- aplicaciones/Professional/restaurant-cart/src/features/order/Order.jsx
+++ aplicaciones/Professional/restaurant-cart/src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import OrderItem from "../order/OrderItem";
 import {
@@ -8,10 +8,17 @@ import {
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
+import { useEffect } from "react";
 
 function Order() {
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const order = useLoaderData();
+
+  const fetcher = useFetcher();
+
+  useEffect(function () {
+    if (!fetcher.data && fetcher.state === "idle") fetcher.load("/menu");
+  }, []);
 
   const {
     id,
@@ -54,7 +61,15 @@ function Order() {
 
       <ul className="divide-y divide-stone-200 border-b border-t">
         {cart.map((item) => (
-          <OrderItem item={item} key={item.pizzaId} />
+          <OrderItem
+            item={item}
+            key={item.pizzaId}
+            isLoadingIngredients={fetcher.state === "loading"}
+            ingredients={
+              fetcher.data?.find((el) => el.id === item.pizzaId).ingredients ??
+              []
+            }
+          />
         ))}
       </ul>
 
