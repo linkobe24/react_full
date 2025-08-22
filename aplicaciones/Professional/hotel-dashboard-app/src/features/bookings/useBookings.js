7e2beb9a7cdd6125c208bf6687a8a13dbbce3355
Manuel Beltran <manuel.beltranc@hotmail.com>
2025-08-21 17:00:22 -0700

    add filter for bookings, using the api

--- aplicaciones/Professional/hotel-dashboard-app/src/features/bookings/useBookings.js
+++ aplicaciones/Professional/hotel-dashboard-app/src/features/bookings/useBookings.js
@@ -1,14 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+
+  //filter
+  const filterValue = searchParams.get("status");
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter], // el segundo valor es el valor de dependencia. cuando ese cambia, re renderiza
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, bookings, error };
