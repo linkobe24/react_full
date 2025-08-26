a5716f3d66e783d57377766b79423087d4f18a10
Manuel Beltran <manuel.beltranc@hotmail.com>
2025-08-25 22:47:28 -0700

    add header and update user data functionality(name, avatar)

--- aplicaciones/Professional/hotel-dashboard-app/src/services/apiAuth.js
+++ aplicaciones/Professional/hotel-dashboard-app/src/services/apiAuth.js
@@ -1,4 +1,5 @@
 import supabase from "./supabase";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
 
 export async function signup({ fullName, email, password }) {
   const { data, error } = await supabase.auth.signUp({
@@ -45,4 +46,33 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1 update password or fullname
+  let updateData;
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(updateData);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+  // 2 updload the avatar image
+  const fileName = `avatar=${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (storageError) throw new Error(error.message);
+  // 3 update avatar in the user
+  const { data: updatedUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${SUPABASE_URL}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+  if (error2) throw new Error(error.message);
+
+  return updatedUser;
 }
