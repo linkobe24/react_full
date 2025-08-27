f615be2a20954ba57d9f6422e1fe97d4b6171f67
Manuel Beltran <manuel.beltranc@hotmail.com>
2025-08-26 23:27:15 -0700

    add errorboundary and fix bug on bookings menu

--- aplicaciones/Professional/hotel-dashboard-app/src/features/bookings/BookingDetail.jsx
+++ aplicaciones/Professional/hotel-dashboard-app/src/features/bookings/BookingDetail.jsx
@@ -18,6 +18,7 @@ import { HiArrowUpOnSquare } from "react-icons/hi2";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
 import { useDeleteBooking } from "./useDeleteBooking";
+import Empty from "../../ui/Empty";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -42,6 +43,7 @@ function BookingDetail() {
   };
 
   if (isLoading) return <Spinner />;
+  if (!booking) return <Empty resourceName="booking" />;
 
   return (
     <>
