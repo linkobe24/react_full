4ee66c516812bb6d74822bdf9a90bc880a42bd1f
Manuel Beltran <manuel.beltranc@hotmail.com>
2025-09-21 12:37:52 -0700

    add react-typescript folder

---
+++ aplicaciones/react-typescript/calculadora-propinas/src/components/TipPercentageForm.tsx
@@ -0,0 +1,51 @@
+import type { Dispatch, SetStateAction } from "react";
+
+const tipOptions = [
+  {
+    id: "tip-10",
+    value: 0.1,
+    label: "10%",
+  },
+  {
+    id: "tip-20",
+    value: 0.2,
+    label: "20%",
+  },
+  {
+    id: "tip-50",
+    value: 0.5,
+    label: "50%",
+  },
+];
+
+type TipPercentageFormProps = {
+  setTip: Dispatch<SetStateAction<number>>;
+  tip: number;
+};
+
+export default function TipPercentageForm({
+  setTip,
+  tip,
+}: TipPercentageFormProps) {
+  return (
+    <div>
+      <h3 className="text-2xl font-black">Propina:</h3>
+
+      <form>
+        {tipOptions.map((tipOption) => (
+          <div key={tipOption.id} className="flex gap-2">
+            <label htmlFor={tipOption.id}>{tipOption.label}</label>
+            <input
+              type="radio"
+              id={tipOption.id}
+              name="tipOption"
+              value={tipOption.value}
+              onChange={(e) => setTip(Number(e.target.value))}
+              checked={tipOption.value === tip}
+            />
+          </div>
+        ))}
+      </form>
+    </div>
+  );
+}
